ef505994ee38ebd43bae0dfbc1d554b3151ff1a9
FabianLucana <70350793+FabianL5@users.noreply.github.com>
2022-06-09 13:26:43 -0500

    2

---
+++ src/Components/Onboarding.js
@@ -0,0 +1,72 @@
+import React , {useState , useRef} from 'react'
+import { FlatList , StatusBar } from 'react-native'
+import {StyleSheet , Animated  , SafeAreaView} from 'react-native'
+import {useNavigation} from "@react-navigation/native";
+import colors from "../Utils/colors";
+
+
+
+import slides from './slides'
+import OnboardingItems from "./OnboardingItems"
+import Paginator from "./Paginator"
+import NextButton from "./NextButton"
+
+export default OnBoarding = () => {
+    
+    const [currentIndex, setcurrentIndex] = useState(0);
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const slidesRef = useRef(null)
+    const navigation = useNavigation();
+
+    const viewableItemsChanged = useRef(({viewableItems}) =>{
+        setcurrentIndex(viewableItems[0].index);
+    }).current;
+
+    const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50}).current;
+
+    const scrollTo = () => {
+        if (currentIndex < slides.length -1 ) {
+            slidesRef.current.scrollToIndex({index:currentIndex+1});
+        }
+        else {
+            //Borrar dos lineas posteriores <3
+            console.log("Onboarding exitoso")
+            navigation.navigate('static');
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle={"light-content"}/>
+            <FlatList 
+            data={slides} 
+            renderItem={({item}) => <OnboardingItems item={item} /> }
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+            bounces={false}
+            keyExtractor={(item)=> item.id}
+            onScroll={Animated.event([{nativeEvent:{contentOffset:{x:scrollX}}}],{
+                useNativeDriver:false,
+            })}
+            scrollEventThrottle={32}
+            onViewableItemsChanged={viewableItemsChanged}
+            viewabilityConfig={viewConfig}
+            ref={slidesRef}
+
+            />
+            <Paginator data={slides} scrollX={scrollX}/>
+            <NextButton scrollTo={scrollTo} percentage={(currentIndex + 1) * (100/slides.length)}/>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:colors.BACKGROUNDCOLOR,
+
+    },
+})
